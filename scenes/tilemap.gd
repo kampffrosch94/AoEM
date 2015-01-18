5f078f08aeb5ec5e4c725f8c4e413bfe19311dc6
kampffrosch <kampffrosch@weg-werf-email.de>
2015-01-18 16:06:44 +0100

    a*-algorithm added needs debugging

--- scenes/tilemap.gd
+++ scenes/tilemap.gd
@@ -8,10 +8,30 @@ var actors = null
 var lastclickedactor = null
 
 func _ready():
-	blockingtiles = [1]
+	blockingtiles = [-1,1]
 	camera = get_node("camera")
 	set_process_input(true)
+
+	#tests for pathfinder
+	print("Beginn Pathfinder tests")
+	var pf = Pathfinder.new(3,3,self)
 	
+
+	#pf.print_all_tiles()
+	
+	var path = pf.findpath(Vector2(0,0), Vector2(0,1))
+	if path == null:
+		print ("Path is null")
+	else:
+		print ("Pathstart" )
+		for part in path:
+			print(part)
+		print ("Pathend" )
+	
+
+
+
+
 func add_actor(actor):
 	if actors == null:
 		actors = [actor]
@@ -21,18 +41,19 @@ func add_actor(actor):
 func is_cell_blocking(pos):
 	return (  blockingtiles.find( get_cell(pos.x,pos.y) )  != -1 )
 
+
 func set_lastclickedactor(actor):
 	lastclickedactor = actor
 
 func _input(event):
-	if((event.type==InputEvent.MOUSE_BUTTON)):
-		if((event.button_index == 1) and event.is_pressed()):
+	if event.type==InputEvent.MOUSE_BUTTON:
+		if (event.button_index == 1) and event.is_pressed():
 			var pos = world_to_map(camera.get_actual_pos(event.pos))
 			var foundactor = null
 			for actor in actors :
 				var actorpos = world_to_map(actor.get_pos())
 				print("Actorpos: ",actorpos)
-				if(pos == actorpos):
+				if pos == actorpos:
 					foundactor = actor 
 					break
 			if foundactor != null:
@@ -44,3 +65,117 @@ func _input(event):
 					print("Move Actor")
 					lastclickedactor.move_to_coord(pos)
 					lastclickedactor = null
+
+
+class Pathfinder:
+	class Tile:
+		var pos = Vector2()
+		var H # estimate of pathcost to goal
+		var G # pathcost of already traveled path
+		var F # G + H
+		var blocked 
+		func _init(startpos, b = false):
+			pos = startpos
+			blocked = b
+			G = 9999999999999
+			F = G 
+	
+	var tiles #dict of all the tiles, key is pos
+	var width
+	var height
+	
+	func _init(width, height, map):
+		self.width = width
+		self.height = height
+		tiles = {}
+		for w in range(-width,width + 1):
+			for h in range(-height,height + 1):
+				var pos = Vector2(w,h)
+				var newtile = Tile.new(pos,map.is_cell_blocking(pos))
+				tiles[pos] = newtile
+	
+	func print_all_tiles():
+		for w in range(-width,width + 1):
+			for h in range(-height,height + 1):
+				var pos = Vector2(w,h)
+				print("Pos: " , tiles[pos].pos , " Blocked: ", tiles[pos].blocked)
+	
+	
+	func distance(a,b): #walking distance between two tiles without obstacles
+		#walking is also possible diagonally
+		var dx = abs(a.x - b.x)
+		var dy = abs(a.y - b.y)
+		return abs(dx - dy)
+	
+	
+	
+	func findpath(startpos,goalpos):
+		var start = tiles[startpos]
+		var goal = tiles[goalpos]
+		var came_from = {}
+		var open = [start]
+		var closed = []
+		
+		start.H = distance(startpos,goalpos)
+		start.G = 0
+		start.F = start.G + start.H
+		
+		while !open.empty():
+			var current = open[0]
+			for tile in open:
+				if tile.F < current.F:
+					current = tile
+			
+			if current == goal:
+				return reconstructpath(goal,came_from)
+			
+			open.erase(current)
+			closed.append(current)
+			
+			for neighbor in get_neighbors(current):
+				if neighbor in closed:
+					continue
+				var tentative_G = current.G + 1
+				
+				if (not neighbor in open) or (neighbor.G < tentative_G) :
+					came_from[neighbor.pos] = current.pos
+					neighbor.G = tentative_G
+					neighbor.H = distance(neighbor.pos,goal.pos)
+					neighbor.F = neighbor.G + neighbor.H
+					if (not neighbor in open):
+						open.append(neighbor)
+		return null
+	
+	func get_tile(pos):
+		if (pos.x in range(-width,width+1)) and (pos.y in range(-height,height+1)) : 
+			return tiles[pos]
+		else:
+			print ("Return null Pos :" , pos)
+			return null
+	
+	func get_neighbors(middle):
+		var neighbors = []
+		
+		for i in range(-1,2): # -1 and 0 and 1
+			for e in range(-1,2):
+				if not (i == 0 and e == 0):
+					neighbors.append( get_tile(middle.pos + Vector2(i,e))) 
+		
+		while neighbors.find(null) != -1:
+			neighbors.erase(null)
+		return neighbors
+		
+
+	func reconstructpath(current,came_from):
+		print ("Path is getting reconstructed")
+		var path = []
+		while(current.pos in came_from.keys()):
+			path.append(current.pos)
+			current = came_from(current)
+		var reversepath = [] # from start to goal
+		for part in reverse(path):
+			reversepath.append(part)
+		return reversepath
+		
+	
+	
